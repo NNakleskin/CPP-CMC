e625b5f814542d265c7826d15bd8e00b123d2e36
Nikita Nakleskin <nakleskin.nv@gmail.com>
2023-03-21 22:40:13 +0300

    Solved up01-5

--- Contest_1/up01-5.cpp
+++ Contest_1/up01-5.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <climits>
+#include <cstring>
+#include <bits/stdc++.h>
 
 
 int main () {
-    int offset;
-    while (std::cin >> std::hex >> offset) {
-        std::cout << std::cin.tellg() << '\n';
-        unsigned int num = 0;
-        unsigned int res = 0;
-        int count = 0;
-        while (std::cin >> std::hex >> num) {
-            std::cout << num;
-            //if (std::cin.tellg () != 2) { break; }
-            res = num + (res << sizeof (unsigned int) * CHAR_BIT / 4);
-            count++;
-            if (count == 4) {
-                break;
-            }
+    std::string res_number;
+    std::string buf;
+    int count = 0;
+    while (std::cin >> buf) {
+        if (buf.length () != 2) {
+            buf.clear ();
+            res_number.clear ();
+            count = 0;
+            continue;
         }
-        std::cout << res << '\n';
-        //if (std::cin.eof ()) { break; }
+        if (count < 4) {
+            count++;
+            res_number += buf;
+        }
+        if (count == 4) {
+            unsigned int res;
+            std::istringstream s (res_number);
+            s >> std::hex >> res;
+            std::cout << res << std::endl;
+            count = 0;
+            res_number.clear ();
+        }
+        buf.clear ();
     }
 }
