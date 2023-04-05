8aea8c171d80c109c2c7ee5c038dbe0ca9c22146
Nikita Nakleskin <nakleskin.nv@gmail.com>
2023-04-06 01:26:51 +0300

    Solved up04-1

--- Contest_4/up04-1.cpp
+++ Contest_4/up04-1.cpp
@@ -3,7 +3,7 @@
 
 void process (const std::vector<uint64_t>& first, std::vector<uint64_t>& second, std::size_t step) {
     auto second_it = second.rbegin ();
-    for (auto it = first.begin (); it != first.end () && second_it != second.rend (); std::advance (it, step)) {
+    for (auto it = first.begin (); it < first.end () && second_it != second.rend (); it += step) {
         *second_it += *it;
         ++second_it;
     }
