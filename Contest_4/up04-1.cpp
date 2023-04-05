e68d5de5f3ca093840dfb6a7ad0a946873f4000e
Nikita Nakleskin <nakleskin.nv@gmail.com>
2023-04-06 01:19:56 +0300

    Start solving up04-1

---
+++ Contest_4/up04-1.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <cstdint>
+
+void process (const std::vector<uint64_t>& first, std::vector<uint64_t>& second, std::size_t step) {
+    auto second_it = second.rbegin ();
+    for (auto it = first.begin (); it != first.end () && second_it != second.rend (); std::advance (it, step)) {
+        *second_it += *it;
+        ++second_it;
+    }
+}
