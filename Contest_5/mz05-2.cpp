dd13bc893d635acf869c4fff7d07a97d5c5b394a
Nikita Nakleskin <nakleskin.nv@gmail.com>
2023-04-10 00:39:53 +0300

    Solved mz05-2

--- Contest_5/mz05-2.cpp
+++ Contest_5/mz05-2.cpp
@@ -4,50 +4,59 @@
 #include <vector>
 
 class Student {
-    private:
-    public:
-    int num;
-    double avg;
+private:
+public:
+    long long num;
+    long long sum;
     std::string name;
-    Student(std::string str, int mark) {
-        name.append(str);
-        num = 0;
-        avg = mark;
+    Student (std::string str, int mark) {
+        name.append (str);
+        num = 1;
+        sum = mark;
     }
 
-    void add_mark(int mark) {
+    void add_mark (int mark) {
         this->num++;
-        this->avg += mark / 2;
+        this->sum += mark;
     }
 
     Student& operator+= (const Student& rhs) {
         this->num = this->num + 1;
-        this->avg = (this->avg + rhs.avg) / 2;
+        this->sum += rhs.sum;
         return *this;
+    }
+    double get_avg () {
+        return (double) sum / (double) num;
     }
 };
 
+bool operator<(const Student& lhs, const Student& rhs) {
+    return lhs.name < rhs.name;
+}
 
-int main() {
+
+int main () {
     std::vector<Student> student_vect;
     std::string str;
     int num;
-    while(std::cin >> str) {
+    while (std::cin >> str) {
         std::cin >> num;
-        const Student tmp(str, num);
+        const Student tmp (str, num);
         bool flag = false;
-        int max = student_vect.size ();
-        for (int i = 0; i < max; i++) {
+        for (size_t i = 0; i < student_vect.size (); i++) {
             if (!student_vect[i].name.compare (str)) {
                 student_vect[i].operator+=(tmp);
                 flag = true;
             }
         }
-        if(flag == false) {
-            student_vect.push_back(tmp);
+        if (flag == false) {
+            student_vect.push_back (tmp);
         }
     }
-    for (int i = 0; i < max; i++) {
-        std::cout << student_vect[i].name << " " << student_vect[i].avg << std::endl;
+
+    stable_sort (student_vect.begin (), student_vect.end ());
+
+    for (size_t i = 0; i < student_vect.size (); i++) {
+        std::cout << student_vect[i].name << " " << student_vect[i].get_avg () << std::endl;
     }
 }
